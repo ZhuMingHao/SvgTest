fb75f505834529b0f761f2e18d633509e0555efd
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2020-06-11 17:33:31 +0800

    Add project files.

---
+++ SvgTest/ViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SvgTest
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {        
+            LoadSvg();
+        }
+        private SVGGenerator generator;
+        private async void LoadSvg()
+        {
+            generator = new SVGGenerator();
+            await generator.LoadSvg();
+            var generationCancellationTokenSource = new CancellationTokenSource();
+            var souce = await generator.GetSourceAsync(generationCancellationTokenSource.Token);
+            SvgSource = new SvgImageSource();
+            await SvgSource.SetSourceAsync(souce);
+        }
+        private SvgImageSource _svgSource;
+        public SvgImageSource SvgSource
+        {
+            get { return _svgSource; }
+            set
+            {
+                _svgSource = value;
+
+            }
+        }
+
+    }
+
+    public class SVGGenerator
+    {
+       
+        private XmlDocument _document;
+        public SVGGenerator()
+        {
+           
+        }
+        public async Task LoadSvg()
+        {
+            _document = new XmlDocument();
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///svg.xml"));
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                _document.Load(stream.AsStream());
+            }
+        }
+        public async Task<IRandomAccessStream> GetSourceAsync(CancellationToken cancellationToken)
+        {
+            return await Task.Run(() =>
+            {
+                using (var stringWriter = new StringWriter())
+                {
+                    XmlWriterSettings settings = new XmlWriterSettings
+                    {
+                        Indent = true,
+                        Encoding = Encoding.UTF8
+                    };
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        using (var xmlTextWriter = XmlWriter.Create(memoryStream, settings))
+                        {
+                            _document.WriteTo(xmlTextWriter);
+
+                            var ramStream = new InMemoryRandomAccessStream();
+                            memoryStream.CopyTo(ramStream.AsStream());
+                            ramStream.Seek(0);
+                            xmlTextWriter.Flush();
+                            return ramStream;
+                        }
+                    }
+                }
+            }, cancellationToken
+            );
+        }
+    }
+}
